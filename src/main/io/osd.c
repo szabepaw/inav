fb91ed13c3a1628511593e92ab3d70f490f39c00
szabepaw <szabepaw@users.noreply.github.com>
2026-04-29 21:59:13 +0200

    fix: repair osd.c include placement and missing #endif
    - Fix include/endif concatenation on line 125
    - Add missing #endif after osdElementCameraRecordIndicator function

--- src/main/io/osd.c
+++ src/main/io/osd.c
@@ -122,7 +122,8 @@
 
 #ifdef USE_HARDWARE_REVISION_DETECTION
 #include "hardware_revision.h"
-#include "fc/camera_control.h"#endif
+#include "fc/camera_control.h"
+#endif
 
 #define VIDEO_BUFFER_CHARS_PAL    480
 #define VIDEO_BUFFER_CHARS_HDZERO 900
